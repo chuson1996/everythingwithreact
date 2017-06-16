fbf3c6a1c74b155dfcf9ba32269ec5c00140f4c8
Chu Hoang Son <chuson1996@gmail.com>
2017-06-16 17:22:17 +0200

    Refactor

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <button onClick={() => alert('It works')}>Hallo</button>
         </div>
+        <Route path="/a" render={() => <h1>AAAAAA</h1>}/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
